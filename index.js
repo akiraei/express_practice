7f813eb493dbcdd38e34a0851644feef7dadcad6
akiraei <akiraeiphoto@gmail.com>
2018-05-16 15:53:18 +0900

    example test

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const randomstring = require('randomstring')
  
 
 const app = express()
@@ -11,8 +12,25 @@ app.use('/static', express.static('public'))
 // ----------------------contents----------------------------------------------------
 
 
-app.get('/', (req, res) => {res.render('index.ejs')})
+const urls = [
+  {
+    slug: randomstring.generate(8),
+    longUrl: 'https://www.naver.com'
+  }
+]
 
+
+app.get('/', (req, res) => {res.render('index.ejs', {urls: urls})})
+
+app.get('/:slug', (req, res) => {
+  const urlItem = urls.find(item => item.slug === req.param.slug)
+  if (urlItem) 
+  {  res.redirect(301, urlItem.longUrl)
+  } else {
+    res.status(404)
+    res.send("404 not found")
+  }
+ })
 
 
 
