214fa484117ad2135da0f901c594393b047afc2c
akiraei <akiraeiphoto@gmail.com>
2018-05-16 15:16:44 +0900

    add morgan

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
+ 
 
 const app = express()
-
-
+app.use(morgan('dev'))
 app.use('/static', express.static('public'))
+
+
 
 // ----------------------contents----------------------------------------------------
 
